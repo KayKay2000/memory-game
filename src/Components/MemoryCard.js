e4e89db33dd0954346b0fe5a3899a3d1276008d0
Kayci Buchanan <92894323+KayKay2000@users.noreply.github.com>
2022-01-28 16:03:06 -0500

    cards able to flip on hover
    Signed-off-by: Kayci Buchanan <92894323+KayKay2000@users.noreply.github.com>

--- src/Components/MemoryCard.js
+++ src/Components/MemoryCard.js
@@ -3,7 +3,12 @@ import './MemoryCard.css';
 
 class MemoryCard extends React.Component { 
     render() {
-        return <div class="MemoryCard"><img src="https://www.digitalcrafts.com/img/logo-wrench-white.png" class="image"/></div>
+        return <div class="MemoryCard">
+                    <div class="MemoryCard__inner">
+                        <div class="MemoryCard__back"><img src="https://www.digitalcrafts.com/img/logo-wrench-white.png" class="image" alt="wrench"/></div>
+                        <div class="MemoryCard--front">∆</div>
+                    </div>
+                </div>
     }}
 
 
